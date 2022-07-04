8a59c825e8dba2fb5a1f902aad7166ca0403d1d8
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-07-04 17:54:03 +0300

    commit

--- src/PictureApi.js
+++ src/PictureApi.js
@@ -12,7 +12,7 @@ export default class PictureApi {
         const MY_API_KEY = "28384939-76d0db34094acd1949cd365d2";
         
         // const response = await 
-        return axios.get(`${BASE_URL}?key=${MY_API_KEY}&q=${this.searchQueryPicture}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}`)
+        return axios.get(`${BASE_URL}?key=${MY_API_KEY}&q=${this.searchQueryPicture}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
             .then(response => {
                     this.incrementPage();
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
+import './css/style.css'
+
 // import axios from "axios";
-// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 import PictureApi from "./PictureApi";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const form = document.querySelector(".search-form");
 const gallery = document.querySelector(".gallery");
 const loadMoreBtn = document.querySelector(".load-more");
 
 const pictureApi = new PictureApi();
+let countPictures = 0;
+
 
 form.addEventListener("submit", onFormSearch);
 
 function onFormSearch(event) {
     event.preventDefault();
+
+    resetGallery()
 
     // const searchForm = form.elements.searchQuery.value.trim();
     // console.log(searchForm);
@@ -19,32 +27,55 @@ function onFormSearch(event) {
     console.log(pictureApi.query);
     
     // fetchPictures(searchForm).then(pictures => console.log(pictures));
-    pictureApi.fetchPictures().then(data => console.log(data));
+    pictureApi.fetchPictures().then(data => {
+        if (pictureApi.query === "") {
+            notiflixFailure();
+        } else {
+            appendPicture(data.hits);
+            countPictures = data.hits.length;
+            Notiflix.Notify.success(`Hooray! We found ${data.totalHits} pictures.`);
+        }
+    }).catch(() => notiflixFailure());
 };
 
-// function createPictureCard(data) {
-//     return data.map(picture =>  `<div class="photo-card">
-//     <img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" />
-//     <div class="info">
-//         <p class="info-item">
-//         <b>${picture.likes}</b>
-//         </p>
-//         <p class="info-item">
-//         <b>${picture.views}</b>
-//         </p>
-//         <p class="info-item">
-//         <b>${picture.comments}</b>
-//         </p>
-//         <p class="info-item">
-//         <b>${picture.downloads}</b>
-//         </p>
-//     </div>
-// </div>`).join("");
+// loadMoreBtn.addEventListener("click", onBtnClick);
+
+// function onBtnClick() {
+
 // }
 
-// function appendPicture(data) {
-//     gallery.insertAdjacentHTML('beforeend',createPictureCard(data));
-// }
-// function notiflixFailure() {
-//     return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again');
-// }
+function createPictureCard(data) {
+    return data.map(picture =>  `<div class="photo-card">
+    <a href="${picture.largeImageURL}">
+    <img class="photo-card__picture" src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" />
+    </a>
+    <div class="info">
+        <p class="info-item">
+        <b>Likes: </b>${picture.likes}
+        </p>
+        <p class="info-item">
+        <b>Views: </b>${picture.views}
+        </p>
+        <p class="info-item">
+        <b>Comments: </b> ${picture.comments}
+        </p>
+        <p class="info-item">
+        <b>Downloads: </b>${picture.downloads}
+        </p>
+    </div>
+</div>`).join("");
+}
+
+function appendPicture(data) {
+    gallery.insertAdjacentHTML('beforeend',createPictureCard(data));
+}
+function notiflixFailure() {
+    return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again');
+}
+function resetGallery() {
+    gallery.innerHTML= '';
+}
+let galleryEl = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+});
